fce2c198f38c92db025cb4d17a11ceabcb81884e
Ezaki Yuriko <e.guriko920@gmail.com>
2024-10-13 16:11:28 +0900

    fix public base path

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -17,12 +17,12 @@ export default function Index() {
           </h1>
           <div className="h-[144px] w-[434px]">
             <img
-              src="/logo-light.png"
+              src="/recipe/logo-light.png"
               alt="Remix"
               className="block w-full dark:hidden"
             />
             <img
-              src="/logo-dark.png"
+              src="/recipe/logo-dark.png"
               alt="Remix"
               className="hidden w-full dark:block"
             />
